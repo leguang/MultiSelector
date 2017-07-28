459baed4caba8a6a13afa90d57239de6ecd0ae7c
langmanleguang <langmanleguang@qq.com>
2017-07-28 14:42:55 +0800

    add dialog.

--- app/src/main/java/cn/itsite/multiselector/demo/MainActivity.java
+++ app/src/main/java/cn/itsite/multiselector/demo/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                         for (CharSequence s : select) {
                             Log.e(TAG, s.toString());
                         }
+
                     }
                 }).show();
 
